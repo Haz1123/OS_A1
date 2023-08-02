7e3719f6f37c610043f5ba59c1b6913984ac3461
Haz1123 <harrison.burr.1@gmail.com>
2023-08-02 13:57:21 +1000

    Added initial reader implementation

--- copier_src/reader.cpp
+++ copier_src/reader.cpp
@@ -5,6 +5,17 @@
 #include "reader.h"
 #include <functional>
 
-reader::reader(const std::string& name, writer& mywriter) {}
+const int read_buffer_size = 10;
 
-void reader::run() {}
+reader::reader(const std::string& name, writer& mywriter)
+    : thewriter(mywriter) {
+    // TODO: Init ifstream properly
+    //this->in = new ifstream();
+    this->in.open(name);
+}
+
+void reader::run() {
+    char* ingest = new char[read_buffer_size]();
+    this->in.read(ingest, read_buffer_size);
+    this->thewriter.append(std::string(ingest));
+}
