e392acd8121f5971d9337006de324facd87d71ca
Haz1123 <harrison.burr.1@gmail.com>
2023-08-02 15:41:10 +1000

    Added command line args

--- copier_src/main.cpp
+++ copier_src/main.cpp
@@ -5,14 +5,20 @@
 #include "reader.h"
 #include "writer.h"
 #include <cstdlib>
+#include <filesystem>
 /* global variables if needed go here */
-int main(int argc, char** argv) {
-    // TODO: Check command line args
-    // TODO: Set command line args
+int main(int argc, char** argv) {`
+    if(argc <= 2) {
+        std::cout << "Program usage: copier.exe [source] [destination]";
+        exit(1);
+    }
+    std::string infile = argv[1];
+    std::string outfile = argv[2];
 
-    std::string infile = "target.in";
-    std::string outfile = "target.out";
-    
+    if( !std::filesystem::exists(infile) ) {
+        std::cout << "File:" << infile << " doesn't exist.";
+    }
+
     writer* write = new writer(outfile);
     reader* read = new reader(infile, *write);
 
